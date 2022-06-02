51b1f370add9690b9834068fe54924ef9f86b70c
Jackson McGuire <jaxmcguire@gmail.com>
2022-06-02 12:56:49 -0400

    Create update fetch request and form conditional rendering

--- public/react/components/SauceDetails.js
+++ public/react/components/SauceDetails.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { MdOutlineArrowBackIos } from "react-icons/md";
+import { FiEdit } from "react-icons/fi"
+import { AiOutlineDelete } from 'react-icons/ai';
+import { EditSauceForm } from './EditSauceForm';
+import apiURL from '../api';
 
-export const SauceDetails = ({sauce, goHome}) => {
+
+export const SauceDetails = ({ sauce, goHome, deleteSauce }) => {
+
+  const updateSauce = async (newSauce) => {
+		try {
+			await fetch(`${apiURL}/sauces`, {
+				method: "PUT", 
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(newSauce)
+			});
+			} catch (err) {
+			console.log("Oh no an error! ", err)
+		}
+		setIsEditing(false)
+	}
+  const [isEditing, setIsEditing] = useState(false)
   return <section className="sauce-details">
     <MdOutlineArrowBackIos className="back-btn" onClick={goHome}/>
     <img src={sauce.image}></img>
@@ -11,6 +32,9 @@ export const SauceDetails = ({sauce, goHome}) => {
       <p>{sauce.category}</p>
     </div>
     <p>{sauce.price}</p>
+    <FiEdit className="back-btn edit" onClick={() => setIsEditing(!isEditing)}></FiEdit>
+    <AiOutlineDelete className = "back-btn edit" onClick={() =>	deleteSauce(sauce.id)}/>
+	  {isEditing ? <EditSauceForm updateSauce={updateSauce} sauce={sauce}/> : <></>}
   </section>
 } 
 	
